73eea1b1ffa6ce13cde0e49e068eeca6c6c74860
Nirav Parekh <nirav55.parekh@gmail.com>
2020-04-20 16:13:13 +0530

    database connection and passport init

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ var routes = require('./routes/index');
 var cors = require('cors');
 var multipart = require('connect-multiParty');
 var multipartMiddleware = multipart();
-    // Setup server
+var session = require("express-session");
+var passport = require("./config/passport");
+var db = require("./models");
+var PORT = process.env.PORT || 8080;
+// Setup server
 var app = express();
 
 // CORS
 app.use(cors());
 
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.use('/', express.static('public'));
 
@@ -26,9 +30,16 @@ app.use('/', express.static('public'));
 app.use(bodyParser.json());
 
 app.use(multipartMiddleware);
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 routes(app);
 
-app.listen(4400, () => {
-    console.log('server is listening');
-}).setTimeout(600000);
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
+}).catch(e => {
+    console.log("Error! in connection with database");
+});
